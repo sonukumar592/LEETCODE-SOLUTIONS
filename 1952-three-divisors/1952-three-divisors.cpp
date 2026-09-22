c4eb48d6ee042042453b5cf9bddf50899c218cbf
sonukumar592 <sk0740576@gmail.com>
2026-09-22 18:35:04 +0530

    Time: 0 ms (100%), Space: 7.7 MB (57.7%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     bool isThree(int n) {
         int count=0;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=n/2;i++){
             if(n%i==0){
                 count++;
             }
         }
-        if(count==3){
+        if((count+1)==3){
             return true;
         }
         return false;
